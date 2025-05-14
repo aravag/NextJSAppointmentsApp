2d16edeee0f82676a9df2dcc1d6ba87b340537d9
Vagan Arakelyan <vra.vagan@gmail.com>
2025-05-14 17:17:57 -0300

    init commit

---
+++ src/app/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <h1>AppointmentsApp</h1>
+            </div>
+        </footer>
+    )
+}
---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>AppointmentsApp</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href='#'>1</a>
+                    </li>
+                    <li>
+                        <a href='#'>2</a>
+                    </li>
+                    <li>
+                        <a href='#'>3</a>
+                    </li>
+                    <li>
+                        <a href='#'>4</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
